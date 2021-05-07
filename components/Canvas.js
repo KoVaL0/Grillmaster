c998bc2133e44f124eabcfea84b90147e5cb66f6
KoVaL0 <56194853+KoVaL0@users.noreply.github.com>
2021-05-07 17:49:30 +0300

    Resolve git conflicts

--- components/Canvas.js
+++ components/Canvas.js
@@ -12,17 +12,10 @@ const Canvas = ({ grillItems }) => {
         <title>background</title>
         <rect fill="#F4F4F4" id="canvas_background" height={height} width={width} y="-1" x="-1" />
       </g>
-<<<<<<< HEAD
       {grillItems.map((item) => (
         <g key={item.id}>
           <title>{item.title}</title>
           <rect stroke="#000" height={item.height} width={item.width} y={item.y} x={item.x} fill="#729C62" rx="0.3em" />
-=======
-      {grillItems.map((item) => {
-        return <g key={item.id}>
-          <title>{item.title}</title>
-          <rect stroke="#000" height={item.height} width={item.width} y={item.y} x={item.x} fill="#729C62" rx="0.3em"/>
->>>>>>> c6167281f87c38b82fc43d0b146773e82d1ea1ad
         </g>
       ))}
     </svg>
